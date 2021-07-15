76ac8aafd7ad01498cef1d241d663f6ce7aefaa2
Matheus Marques <mmarxdev@gmail.com>
2021-07-15 15:28:04 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import SearchInput from "./SearchInput";
+
+const api = "https://kitsu.io/api/edge/"
+
+
+function App() {
+  const [info, setInfo] = useState({});
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+      if(text){
+        fetch(`${api}anime?filter[text]=${text}&page[limit]=12`)
+
+        .then((response)=> response.json())
+        .then((response) => {
+          setInfo(response)
+        })
+      }
+  }, [text]);
+
+  return (
+    <div className="App">
+      <h1>Buscar Anime</h1>
+      <SearchInput value={text} onChange={(search) => setText(search)} />
+
+      {info.data &&(
+        <ul className="animes-list">
+          {info.data.map((anime) => (
+            <li key={anime.id}>
+                <img src={anime.attributes.posterImage.small} alt={anime.attributes.canonicalTitle}/>
+                {anime.attributes.canonicalTitle}
+            </li>
+          ))}
+        </ul>
+      )}
+
+    </div>
+  );
+}
+export default App;
+
+
